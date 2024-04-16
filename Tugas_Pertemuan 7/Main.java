f73497269589791cd40be4f3084baed16341a10b
CahyaMutaqin <144780074+CahyaMutaqin@users.noreply.github.com>
2024-04-17 06:55:39 +0700

    Add files via upload

---
+++ Tugas_Pertemuan 7/Main.java
@@ -0,0 +1,62 @@
+package main;
+
+import java.util.Arrays;
+
+class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+}
+
+public class Main {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopwatch = new StopWatch();
+
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100000);
+        }
+
+        stopwatch.start();
+
+        selectionSort(arr);
+
+        stopwatch.stop();
+
+        long elapsedTime = stopwatch.getElapsedTime();
+
+        System.out.println("Array yang telah diurutkan: " + Arrays.toString(arr));
+        System.out.println("Waktu yang diperlukan untuk mengurutkan 100.000 angka menggunakan selection sort: " + elapsedTime + " milidetik");
+    }
+}
